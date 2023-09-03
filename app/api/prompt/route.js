d9851007e7673fda32698b05f9699768ef4eaf5b
Mohamed Adel <mohamed@chiliz.com>
2023-09-03 17:27:43 +0200

    showing the prompts data feed

---
+++ app/api/prompt/route.js
@@ -0,0 +1,15 @@
+import { connectToDatabase } from "@utils/database";
+import Prompt from "@models/Prompt";
+
+
+export const GET = async (req) => {
+    try {
+        await connectToDatabase();
+
+        const prompts = await Prompt.find({}).populate('creator');
+
+        return new Response(JSON.stringify(prompts), { status: 200 });
+    } catch (error) {
+        return new Response(JSON.stringify(error), { status: 500 });
+    }
+}
